7fa47d62e89a282eb93b6d399509b5a5806ce712
JiuNian3219 <qin0yu0lou@gmail.com>
2025-11-20 16:41:36 +0800

    feat(home): 添加图片对比滑动组件并重构初始页面布局
    - 新增CompareSlider组件实现图片对比滑动功能
    - 重构首页布局，支持响应式设计

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,9 @@
+import previewScenery from "@/assets/preview-scenery.jpg";
+import pixelPreviewScenery from "@/assets/preview-scenery.pixel.png";
+import CompareSlider from "@/components/CompareSlider";
+import { useIsMobile } from "@/hooks/useIsMobile";
 import { parseLocaleFromPath, withLocalePath } from "@/utils/locale";
-import { Button, Typography } from "antd";
+import { Button, Col, Flex, Row, Typography } from "antd";
 import React from "react";
 import { Trans, useTranslation } from "react-i18next";
 import { useNavigate } from "react-router-dom";
@@ -10,15 +14,20 @@ const { Title } = Typography;
 const Home: React.FC = () => {
   const navigate = useNavigate();
   const locale = parseLocaleFromPath(window.location.pathname);
+  const isMobile = useIsMobile();
   const { t } = useTranslation("home");
   const handleStartClick = () => {
     navigate(withLocalePath(locale, "/creator"));
   };
 
   return (
-    <div className={styles.container}>
-      <div className={styles.card}>
-        <div>
+    <Row
+      justify="center"
+      align="middle"
+      className={styles.container}
+    >
+      {isMobile && (
+        <Col>
           <Title className={styles.title}>
             <Trans
               i18nKey="title"
@@ -28,20 +37,51 @@ const Home: React.FC = () => {
               }}
             />
           </Title>
-        </div>
-
-        <Button
-          title={t("start_button")}
-          color="primary"
-          variant="outlined"
-          size="large"
-          onClick={handleStartClick}
-          className={styles.startButton}
+        </Col>
+      )}
+      <Col
+        span={isMobile ? 24 : 12}
+        className={styles.imagePreview}
+      >
+        <CompareSlider
+          leftSrc={pixelPreviewScenery}
+          rightSrc={previewScenery}
+          alt="pixel-artist"
+        />
+      </Col>
+      <Col
+        span={isMobile ? 24 : 12}
+        className={styles.content}
+      >
+        <Flex
+          vertical
+          align="center"
+          gap={80}
         >
-          {t("start_button")}
-        </Button>
-      </div>
-    </div>
+          {!isMobile && (
+            <Title className={styles.title}>
+              <Trans
+                i18nKey="title"
+                ns="home"
+                components={{
+                  1: <span />,
+                }}
+              />
+            </Title>
+          )}
+          <Button
+            title={t("start_button")}
+            color="primary"
+            variant="outlined"
+            size="large"
+            onClick={handleStartClick}
+            className={styles.startButton}
+          >
+            {t("start_button")}
+          </Button>
+        </Flex>
+      </Col>
+    </Row>
   );
 };
 
